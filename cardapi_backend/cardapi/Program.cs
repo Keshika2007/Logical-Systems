641baa8a17fc9c1d9e62f24ddd1261b0f5d366d4
Keshika2007 <136632857+Keshika2007@users.noreply.github.com>
2024-09-07 23:34:53 +0800

    Add files via upload

---
+++ cardapi_backend/cardapi/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseRouting();
+
+app.UseCors("AllowAll"); // Apply the CORS policy
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
